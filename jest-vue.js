221b1e0139bb2f8933d201c37f3cc119e3d7fc78
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-09-02 20:06:33 +0100

    refactor: extract functions to their own files

--- jest-vue.js
+++ jest-vue.js
@@ -1,59 +1,11 @@
 const vueCompiler = require('vue-template-compiler')
 const babel = require('babel-core')
-const sourceMap = require('source-map')
-const path = require('path')
-const compileTemplate = require('./template-compiler')
+const compileTemplate = require('./lib/template-compiler')
+const generateSourceMap = require('./lib/generate-source-map')
+const addTemplateMapping = require('./lib/add-template-mapping')
+
 const convert = require('convert-source-map')
-const fs = require('fs')
 const splitRE = /\r?\n/g
-
-function generateSourceMap (script, output, filePath, content, inputMap) {
-  var hashedFilename = path.basename(filePath)
-  var map = new sourceMap.SourceMapGenerator()
-  map.setSourceContent(hashedFilename, content)
-  // check input source map from babel/coffee etc
-  var inputMapConsumer = inputMap && new sourceMap.SourceMapConsumer(inputMap)
-  var generatedOffset = (output ? output.split(splitRE).length : 0) + 1
-  script.split(splitRE).forEach(function (line, index) {
-    var ln = index + 1
-    var originalLine = inputMapConsumer
-      ? inputMapConsumer.originalPositionFor({ line: ln, column: 0 }).line
-      : ln
-    if (originalLine) {
-      map.addMapping({
-        source: hashedFilename,
-        generated: {
-          line: ln + generatedOffset,
-          column: 0
-        },
-        original: {
-          line: originalLine,
-          column: 0
-        }
-      })
-    }
-  })
-  map._hashedFilename = hashedFilename
-  return map
-}
-
-function addTemplateMapping (content, parts, output, map, beforeLines) {
-  var afterLines = output.split(splitRE).length
-  var templateLine = content.slice(0, parts.template.start).split(splitRE).length
-  for (; beforeLines < afterLines; beforeLines++) {
-    map.addMapping({
-      source: map._hashedFilename,
-      generated: {
-        line: beforeLines,
-        column: 0
-      },
-      original: {
-        line: templateLine,
-        column: 0
-      }
-    })
-  }
-}
 
 module.exports = {
   process (src, path) {
