56e1bc9486463e46b1e188c11da7dc469375f522
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-09-13 14:53:57 +0100

    test: run tests in a temporary directory

---
+++ test.sh
@@ -0,0 +1,10 @@
+rm -rf temp-dir
+mkdir temp-dir
+cp -R  test temp-dir/test
+cp -R  lib temp-dir/lib
+cp -R  node_modules temp-dir/node_modules
+cp jest-vue.js temp-dir/jest-vue.js
+cp package.json temp-dir/package.json
+cp .babelrc temp-dir/.babelrc
+cd temp-dir
+yarn unit
