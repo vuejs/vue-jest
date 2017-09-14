621ede071486939dc712f2db50174df7c14ef35b
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-09-14 15:12:00 +0100

    perf: add getCacheKey function

--- jest-vue.js
+++ jest-vue.js
@@ -1,5 +1,7 @@
 const process = require('./lib/process')
+const getCacheKey = require('./lib/get_cache_key')
 
 module.exports = {
-  process: process
+  process: process,
+  getCacheKey: getCacheKey
 }
