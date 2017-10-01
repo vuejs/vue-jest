7ce2ee638304281808e5b3695e488c39c8d406c2
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-10-01 09:16:03 +0100

    fix: get tsconfig when compiling typescript

--- test.sh
+++ test.sh
@@ -6,5 +6,6 @@ cp -R  node_modules temp-dir/node_modules
 cp jest-vue.js temp-dir/jest-vue.js
 cp package.json temp-dir/package.json
 cp .babelrc temp-dir/.babelrc
+cp tsconfig.json temp-dir/tsconfig.json
 cd temp-dir
 yarn unit
--- test/resources/TypeScript.vue
+++ test/resources/TypeScript.vue
@@ -1,13 +1,18 @@
 <template>
-    <div></div>
+    <div><basic /></div>
 </template>
 
 <script lang="ts">
+import Basic from './Basic'
+
 export default {
     computed: {
         exclamationMarks(): string {
             return  'string'
         }
+    },
+    components: {
+      Basic
     }
 };
 </script>
