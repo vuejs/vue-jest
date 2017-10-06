5ed30beafeccf5c2ad968af943cbcef8cbe97527
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-10-06 07:18:50 +0100

    fix: remove babel from typescript pipeline

--- test/resources/TypeScript.vue
+++ test/resources/TypeScript.vue
@@ -1,18 +1,13 @@
 <template>
-    <div><basic /></div>
+    <div></div>
 </template>
 
 <script lang="ts">
-import Basic from './Basic'
-
 export default {
     computed: {
         exclamationMarks(): string {
             return  'string'
         }
-    },
-    components: {
-      Basic
     }
 };
 </script>
