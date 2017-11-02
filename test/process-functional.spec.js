788c9a212a0b6c83ec6419af7721c9d3669ab137
Alberto Gualis <agualis@gmail.com>
2017-11-02 09:31:59 +0100

    Fix linter errors

--- test/process-functional.spec.js
+++ test/process-functional.spec.js
@@ -1,7 +1,6 @@
 import extractProps from '../lib/process-functional'
 
 describe('when extracting props with props. prefix from functional template content', () => {
-
   it('extracts interpolated props ', () => {
     const content = '<div> {{props.msg1 }} {{props.msg2}}</div>'
 
