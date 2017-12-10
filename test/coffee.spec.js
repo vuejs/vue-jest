7aaa229e3f91c3f94dc40f8c4bc274f25c5e9d8f
Wataru Oguchi <wataru.oguchi.0to1@gmail.com>
2017-12-10 13:38:41 -0800

    Transpile coffeescript written in ES6

--- test/coffee.spec.js
+++ test/coffee.spec.js
@@ -1,6 +1,8 @@
 import { shallow } from 'vue-test-utils'
 import Coffee from './resources/Coffee.vue'
 import CoffeeScript from './resources/CoffeeScript.vue'
+import CoffeeES6 from './resources/CoffeeES6.vue'
+import CoffeeScriptES6 from './resources/CoffeeScriptES6.vue'
 
 test('processes .vue file with lang set to coffeescript', () => {
   shallow(Coffee)
@@ -8,4 +10,12 @@ test('processes .vue file with lang set to coffeescript', () => {
 
 test('processes .vue file with lang set to coffeescript', () => {
   shallow(CoffeeScript)
+})
+
+test('processes .vue file with lang set to coffeescript', () => {
+  shallow(CoffeeES6)
+})
+
+test('processes .vue file with lang set to coffeescript', () => {
+  shallow(CoffeeScriptES6)
 })
