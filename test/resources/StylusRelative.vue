2576a2b4eb51686f0d0bd14753c5c5251042b080
Mike Dvorscak <mike.dvorscak@gmail.com>
2018-02-05 21:47:03 -0600

    Add failing test

---
+++ test/resources/StylusRelative.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="testB"></div>
+</template>
+
+<script>
+export default {
+  name: 'Button',
+}
+</script>
+
+
+<style lang="styl" module>
+  @import '../relative/resource';
+  .testB {
+    margin: standard-space;
+  }
+</style>
