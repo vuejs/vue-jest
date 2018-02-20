c26db3b5156faeda7d9cda6cd7ac5c54efefe0ed
Kengo Hamasaki <k.hamasaki@gmail.com>
2018-02-19 22:58:30 -0800

    fix: avoid parsing LESS, and PostCSS (#58)

---
+++ test/resources/Less.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="testLess"></div>
+</template>
+
+<style lang="less">
+  @import "./styles/transitions";
+
+  .testLess {
+    background-color: red;
+  }
+</style>
+
+<style lang="less">
+  @primary-color: #333;
+</style>
---
+++ test/resources/PostCss.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="testPcss"></div>
+</template>
+
+<style lang="postcss">
+  @import "./styles/transitions";
+
+  .testPcss {
+    background-color: purple;
+  }
+</style>
+
+<style lang="pcss">
+  /* This syntax is for postcss-custom-properties */
+  --primary-color: green;
+
+  /* This syntax is for postcss-nesting, but invalid as Pure CSS */
+  body {
+    @media screen {
+      background-color: grey;
+    }
+  }
+</style>
