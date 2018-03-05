d9afe4b593d8df7717d0f3ddd445bbd2cb12132e
eddyerburgh <edward.yerburgh@gmail.com>
2018-03-05 07:07:40 +0000

    fix: remove cacheing

--- vue-jest.js
+++ vue-jest.js
@@ -1,7 +1,5 @@
 const process = require('./lib/process')
-const getCacheKey = require('./lib/get_cache_key')
 
 module.exports = {
-  process: process,
-  getCacheKey: getCacheKey
+  process: process
 }
