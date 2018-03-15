f9f9f151ba162caabe32ecbfc92a77815f6e2096
rchaser53 <tayoshizawa29@gmail.com>
2018-03-15 22:51:08 +0900

    suppress the error when using sass, postcss, less module

---
+++ test/resources/SassModule.vue
@@ -0,0 +1,15 @@
+<template>
+  <div :class="$style.testA"></div>
+</template>
+
+<style module lang="scss">
+  @import "./styles/transitions";
+
+  .testA {
+    background-color: red;
+  }
+</style>
+
+<style module lang="sass">
+  $primary-color: #333
+</style>
