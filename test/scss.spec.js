6c8f485b9ae3ebc7f05bbe383a3d716abb07e5af
Brian Tu <feihcsim824@gmail.com>
2018-03-16 11:42:33 -0400

    satisfy scss spec

--- test/scss.spec.js
+++ test/scss.spec.js
@@ -2,7 +2,7 @@ import { shallow } from 'vue-test-utils'
 import Sass from './resources/Sass.vue'
 import SassModule from './resources/SassModule.vue'
 
-describe('processes .vue file with Stylus style', () => {
+describe('processes .vue file with scss style', () => {
   it('does not error on scss or sass', () => {
     const wrapper = shallow(Sass)
     expect(wrapper.classes()).toContain('testA')
