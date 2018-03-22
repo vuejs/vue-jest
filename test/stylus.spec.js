dae9ab697e814b2d3b3b7aa117a78371facd0c34
eddyerburgh <edward.yerburgh@gmail.com>
2018-03-22 22:28:12 +0000

    feat: add expirmentalCSSCompile flag

--- test/stylus.spec.js
+++ test/stylus.spec.js
@@ -1,6 +1,9 @@
 import { shallow } from 'vue-test-utils'
 import Stylus from './resources/Stylus.vue'
 import StylusRelative from './resources/StylusRelative.vue'
+import { resolve } from 'path'
+import { readFileSync } from 'fs'
+import jestVue from '../vue-jest'
 
 describe('processes .vue file with Stylus style', () => {
   let wrapper
@@ -22,5 +25,15 @@ describe('processes .vue file with Stylus style', () => {
 
   it('should handle relative imports', () => {
     expect(() => shallow(StylusRelative)).not.toThrow()
+  })
+
+  it('does not attempty to compile if experimentalStyles flag is passed', () => {
+    const filePath = resolve(__dirname, './resources/Basic.vue')
+    const fileString = readFileSync(filePath, { encoding: 'utf8' })
+    const fileStringWithInvalidSass = `
+      ${fileString}
+      <style lang="stylus">@something</style>
+    `
+    jestVue.process(fileStringWithInvalidSass, filePath, { globals: { 'vue-jest': { experimentalCSSCompile: false }}})
   })
 })
