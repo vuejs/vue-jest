aa94fefa9c1d68c83fd8d833e2302f0134f74a0b
Jeremy "Jay" Zahner <me@jeremyzahner.com>
2018-04-10 07:20:11 +0200

    fix: support styles when combining modules and SCSS (#79)
    Fixes #63

--- test/scss.spec.js
+++ test/scss.spec.js
@@ -3,12 +3,20 @@ import Sass from './resources/Sass.vue'
 import SassModule from './resources/SassModule.vue'
 
 describe('processes .vue file with scss style', () => {
-  it('does not error on scss or sass', () => {
-    const wrapper = shallow(Sass)
-    expect(wrapper.classes()).toContain('testA')
+  it('does not error on scss/sass', () => {
+    expect(() => shallow(Sass)).not.toThrow()
   })
-
   it('does not error on scss/sass module', () => {
     expect(() => shallow(SassModule)).not.toThrow()
+  })
+})
+
+describe('processes .vue files which combine scss/sass and modules', () => {
+  it('does inject classes to $style', () => {
+    const wrapper = shallow(SassModule)
+    expect(wrapper.exists()).toBe(true)
+    expect(wrapper.vm.$style).toBeDefined()
+    expect(wrapper.vm.$style.testA).toBeDefined()
+    expect(wrapper.vm.$style.testB).toBeDefined()
   })
 })
