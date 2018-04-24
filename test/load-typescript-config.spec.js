89d2eb41c7c3c76d1bf7b2e336d9f2b0f3fc1a44
Daniel Herman <dherman@factset.com>
2018-04-24 14:37:22 -0400

    feat: allow specifying the tsconfig file as a jest global

--- test/load-typescript-config.spec.js
+++ test/load-typescript-config.spec.js
@@ -4,7 +4,9 @@ import {
   createReadStream,
   createWriteStream,
   readFileSync,
-  renameSync
+  writeFileSync,
+  renameSync,
+  unlinkSync
 } from 'fs'
 import clearModule from 'clear-module'
 import cache from '../lib/cache'
@@ -20,7 +22,7 @@ describe('load-typescript-config.js', () => {
     const tempPath = resolve(__dirname, '../.renamed')
     renameSync(tsconfigPath, tempPath)
 
-    const tsConfig = loadTypescriptConfig()
+    const tsConfig = loadTypescriptConfig({})
 
     try {
       expect(tsConfig).toEqual(defaultConfig)
@@ -34,16 +36,34 @@ describe('load-typescript-config.js', () => {
     expect(tsconfigCachedConfig).toEqual(defaultConfig)
   })
 
+  it('returns the tsconfig specified in jest globals', () => {
+    const jestGlobalTsConfigPath = resolve(__dirname, '../tsconfig.jest.json')
+
+    writeFileSync(jestGlobalTsConfigPath, JSON.stringify({
+      allowJs: false
+    }))
+
+    const jestGlobalTsConfig = JSON.parse(readFileSync(jestGlobalTsConfigPath, { encoding: 'utf8' }))
+
+    const tsconfig = loadTypescriptConfig({
+      tsConfigFile: jestGlobalTsConfigPath
+    })
+
+    expect(tsconfig).toEqual(jestGlobalTsConfig)
+
+    unlinkSync(jestGlobalTsConfigPath)
+  })
+
   it('reads default tsconfig if there is tsconfig.json', () => {
     const tsconfigPath = resolve(__dirname, '../tsconfig.json')
     const tsconfigCopiedPath = resolve(__dirname, '../.tsconfig.json_cp')
     createReadStream(tsconfigPath).pipe(createWriteStream(tsconfigCopiedPath))
     const tsconfigOriginal = JSON.parse(readFileSync(tsconfigPath, { encoding: 'utf8' }))
-    const tsconfig = loadTypescriptConfig()
+    const tsconfig = loadTypescriptConfig({})
     expect(tsconfig).toEqual(tsconfigOriginal)
     const tempPath = resolve(__dirname, '../.renamed')
     renameSync(tsconfigCopiedPath, tempPath)
-    const tsconfigCached = loadTypescriptConfig()
+    const tsconfigCached = loadTypescriptConfig({})
     try {
       expect(tsconfig).not.toBe(tsconfigCached)
       expect(tsconfig).toEqual(tsconfigCached)
