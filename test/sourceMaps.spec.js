c6a65a3fbc2bdf987114a0285523971d769f6b12
eddyerburgh <edward.yerburgh@gmail.com>
2018-11-25 20:15:27 +0000

    test: use snpashot tests

--- test/sourceMaps.spec.js
+++ test/sourceMaps.spec.js
@@ -1,26 +1,6 @@
 import { resolve } from 'path'
 import { readFileSync } from 'fs'
-import clearModule from 'clear-module'
-import cache from '../lib/cache'
 import jestVue from '../vue-jest'
-
-beforeEach(() => {
-  cache.flushAll()
-  clearModule.all()
-})
-
-const getSourceMaps = code => {
-  const sourceMapBase64 = /\/\/# sourceMappingURL=data:application\/json;charset=utf-8;base64,(.*)/gim
-
-  let matches
-  let values = []
-
-  while ((matches = sourceMapBase64.exec(code)) !== null) {
-    values.push(JSON.parse(Buffer.from(matches[1], 'base64').toString('ascii')))
-  }
-
-  return values
-}
 
 test('generates source maps for .vue files', () => {
   const filePath = resolve(__dirname, './resources/Basic.vue')
@@ -30,10 +10,7 @@ test('generates source maps for .vue files', () => {
     moduleFileExtensions: ['js', 'vue']
   })
 
-  const [template, js] = getSourceMaps(code)
-
-  expect(js.sources[0]).toBe('Basic.vue')
-  expect(template.sources[0]).toBe('Basic.vue')
+  expect(code).toMatchSnapshot()
 })
 
 test('generates source maps using src attributes', () => {
@@ -44,8 +21,5 @@ test('generates source maps using src attributes', () => {
     moduleFileExtensions: ['js', 'vue']
   })
 
-  const [template, js] = getSourceMaps(code)
-
-  expect(js.sources[0]).toBe('BasicSrc.js')
-  expect(template.sources[0]).toBe('SourceMapsSrc.vue')
+  expect(code).toMatchSnapshot()
 })
