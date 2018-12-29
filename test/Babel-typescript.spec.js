6fc24fc587ca8b43e87ab2de62c428c7c5c5c290
Majedul Hoque <rumanbsl@gmail.com>
2018-12-29 17:22:03 +0200

    create  test covering babel upgrade

--- test/Babel-typescript.spec.js
+++ test/Babel-typescript.spec.js
@@ -12,16 +12,27 @@ beforeEach(() => {
   cache.flushAll()
   clearModule.all()
 })
-test.only('@babel/preset-typescript fix', () => {
+test.only('babel configuration expects filename [default: "unknown"] to compile', () => {
   const babelRcPath = resolve(__dirname, '../.babelrc')
   const babelRcOriginal = readFileSync(babelRcPath, { encoding: 'utf8' })
   const filePath = resolve(__dirname, './resources/TypeScript.vue')
   const fileString = readFileSync(filePath, { encoding: 'utf8' })
 
-  writeFileSync(babelRcPath, '{"presets": ["@babel/env","@babel/typescript"],"plugins": ["istanbul"]}')
-  const output = jestVue.process(fileString, filePath)
-  // coverageData.hash is added by babel-plugin-istanbul, added to root .babelrc for this test only
-  expect(output.code).toContain('coverageData.hash')
+  writeFileSync(babelRcPath, '{"presets": ["@babel/env"],"plugins": ["istanbul"]}')
+
+  try {
+    expect(() => jestVue.process(fileString, filePath, undefined, undefined, { filename: 'unknown' })).not.toThrow()
+  } catch (err) {
+    writeFileSync(babelRcPath, babelRcOriginal)
+    throw err
+  }
+
+  try {
+    expect(() => jestVue.process(fileString, filePath, undefined, undefined, { filename: undefined })).toThrow()
+  } catch (err) {
+    writeFileSync(babelRcPath, babelRcOriginal)
+    throw err
+  }
 
   writeFileSync(babelRcPath, babelRcOriginal)
 })
