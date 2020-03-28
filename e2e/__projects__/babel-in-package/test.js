8549590a3202e312077952a5539609a4800b508b
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-03-28 20:03:31 +1000

    refactor: tests

--- e2e/__projects__/babel-in-package/test.js
+++ e2e/__projects__/babel-in-package/test.js
@@ -1,19 +1,38 @@
-import { mount } from '@vue/test-utils'
+import { createApp, h } from 'vue'
+import { resolve } from 'path'
+import { readFileSync } from 'fs'
+
 import TypeScript from './components/TypeScript.vue'
 import Basic from './components/Basic.vue'
 import Coffee from './components/Coffee.vue'
 
+function mount(Component, props, slots) {
+  document.getElementsByTagName('html')[0].innerHTML = ''
+  const el = document.createElement('div')
+  el.id = 'app'
+  document.body.appendChild(el)
+  const Parent = {
+    render() {
+      return h(Component, props, slots)
+    }
+  }
+  const app = createApp(Parent).mount(el)
+}
+
 test('processes .vue files', () => {
-  const wrapper = mount(Basic)
-  wrapper.vm.toggleClass()
+  mount(Basic)
+  expect(document.querySelector('h1').textContent).toBe(
+    'Welcome to Your Vue.js App'
+  )
 })
 
 test('processes .vue file with lang set to coffee', () => {
-  const wrapper = mount(Coffee)
-  expect(wrapper.vm).toBeTruthy()
+  mount(Coffee)
+  expect(document.querySelector('h1').textContent).toBe('Coffee')
 })
 
 test('processes .vue files with lang set to typescript', () => {
   const wrapper = mount(TypeScript)
-  expect(wrapper.vm).toBeTruthy()
+  expect(document.querySelector('#parent').textContent).toBe('Parent')
+  expect(document.querySelector('#child').textContent).toBe('Child')
 })
