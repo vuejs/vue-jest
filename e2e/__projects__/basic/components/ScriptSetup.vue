560ee7c030c754a2796b0cb4ea8bf923777414ae
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-10-05 22:08:39 +1000

    wip: script setup

---
+++ e2e/__projects__/basic/components/ScriptSetup.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="increase">Count: {{ num }}</button>
+  <span>{{ msg }}</span>
+</template>
+
+<script setup="props, { emit }" lang="ts">
+import { ref } from 'vue'
+export const num = ref(5)
+const greet = () => console.log('greet')
+export const increase = () => {
+  greet()
+  num.value++
+}
+export const msg = 'hello world'
+</script>
