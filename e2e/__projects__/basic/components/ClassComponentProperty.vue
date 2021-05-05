76dfcad77cdb8246da8a836e59ed08996b579954
Evertvdw <evertvanderweit@gmail.com>
2021-05-05 07:52:07 +0200

    fix: support for class component mixins (#334)
    Fix for using class component with mixins and also for importing
    Vue from vue-property-decorator instead of vue-class-component.
    
    Co-authored-by: Evert van der Weit <evert@mett.nl>

---
+++ e2e/__projects__/basic/components/ClassComponentProperty.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1 data-computed>{{ computedMsg }}</h1>
+    <h2 data-props>{{ msg }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Prop } from 'vue-property-decorator'
+
+export default class ClassComponent extends Vue {
+  dataText: string = 'Hello'
+
+  @Prop() msg!: string
+
+  get computedMsg(): string {
+    return `Message: ${this.dataText}`
+  }
+}
+</script>
