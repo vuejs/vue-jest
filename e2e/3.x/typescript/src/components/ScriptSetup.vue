800b6f480c8df6a48428cb53d02021a378431aff
Benedikt Throner <beneee@users.noreply.github.com>
2021-10-25 11:56:31 +0200

    feat: support TS with script setup (#402)

---
+++ e2e/3.x/typescript/src/components/ScriptSetup.vue
@@ -0,0 +1,18 @@
+<template>
+  <button @click="increase">Count: {{ num }}</button>
+  <Basic />
+  <span>{{ msg! }}</span>
+</template>
+
+<script setup lang="ts">
+import Basic from './Basic.vue'
+import { ref } from 'vue'
+
+const num = ref(5)
+const greet = () => console.log('greet')
+const increase = () => {
+  greet()
+  num.value++
+}
+const msg = 'hello world'
+</script>
