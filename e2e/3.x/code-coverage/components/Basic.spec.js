46a7edcc8f68ebf129abab8ed58823dea34cedcc
James Harris <james.harris@trutify.com>
2021-12-03 15:22:50 +0000

    chore: add reproducible e2e test for #422

---
+++ e2e/3.x/code-coverage/components/Basic.spec.js
@@ -0,0 +1,19 @@
+import { assert } from '@vue/compiler-core'
+import { shallowMount } from '@vue/test-utils'
+
+import Basic from './Basic.vue'
+
+describe('Basic', () => {
+  it('renders', () => {
+    expect.assertions(4)
+
+    const wrapper = shallowMount(Basic)
+    const div = wrapper.find('div')
+    const h1 = wrapper.find('h1')
+
+    expect(wrapper.element).toMatchSnapshot()
+    expect(div.attributes('class')).toBe('hello')
+    expect(h1.attributes('class')).toBe('blue')
+    expect(h1.text()).toBe('Hello World')
+  })
+})
---
+++ e2e/3.x/code-coverage/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.js$': 'babel-jest'
+  },
+  collectCoverageFrom: ['components/*.vue'],
+  coverageReporters: ['lcov', 'text']
+}
