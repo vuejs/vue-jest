06d6cea725625b0a20bcb0c58863f8d7de8e0c8d
Kirill Zaytsev <zaytsev.cmath10@gmail.com>
2022-05-22 17:59:59 +0300

    fix: Less relative path in imports

--- e2e/2.x/style/components/Less.vue
+++ e2e/2.x/style/components/Less.vue
@@ -1,9 +1,18 @@
 <template>
-  <div />
+  <div>
+    <div :class="$style.a">a</div>
+    <div :class="$style.b">b</div>
+    <div :class="$style.c">c</div>
+    <div :class="$style.d">d</div>
+  </div>
 </template>
 
 <style module lang="less">
+@import './styles/less-a.less';
 .a {
   background-color: @primary-color;
+}
+.b {
+  color: @primary-color;
 }
 </style>
