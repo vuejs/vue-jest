571831aa7b1c91ab2c09bcb1c72222661f6f7f84
Felix Graf <git@felixgraf.dev>
2022-07-05 12:55:33 +0200

    refactor: remove unnecessary refTransform

--- packages/vue2-jest/lib/process.js
+++ packages/vue2-jest/lib/process.js
@@ -50,7 +50,6 @@ function processScriptSetup(descriptor, filePath, config) {
   const vueJestConfig = getVueJestConfig(config)
   const content = compileScript(descriptor, {
     id: filePath,
-    refTransform: true,
     ...vueJestConfig.compilerOptions
   })
   const contentMap = mapLines(descriptor.scriptSetup.map, content.map)
@@ -83,7 +82,6 @@ function processTemplate(descriptor, filename, config) {
   if (scriptSetup) {
     const scriptSetupResult = compileScript(descriptor, {
       id: filename,
-      refTransform: true,
       ...vueJestConfig.compilerOptions
     })
     bindings = scriptSetupResult.bindings
