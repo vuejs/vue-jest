36c6d970ed3cd29797791fa4e8c6b2fc2b5c34d4
Renan Andrade <renansoareess@gmail.com>
2023-03-02 23:02:36 +0100

    fix: script and script setup in same vue file (#536)

---
+++ e2e/2.x/basic/components/ScriptAndScriptSetup.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="increase">Products: {{ products }}</button>
+    <Basic />
+    <span>{{ msg }}</span>
+  </div>
+</template>
+
+<script>
+import Basic from './Basic.vue'
+</script>
+
+<script setup>
+import { ref } from 'vue'
+
+const products = ref(10)
+const greet = () => console.log('greet')
+const increase = () => {
+  greet()
+  num.value++
+}
+const msg = 'hello world'
+</script>
